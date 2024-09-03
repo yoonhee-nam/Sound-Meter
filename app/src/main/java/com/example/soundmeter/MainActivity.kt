2e7fad197f52c2b8fe7c08dffc2d59952f5f69e1
yoonhee <flvps123@gmail.com>
2024-09-03 14:32:40 +0900

    [UI] circle 구간별 색 적용

--- app/src/main/java/com/example/soundmeter/MainActivity.kt
+++ app/src/main/java/com/example/soundmeter/MainActivity.kt
@@ -11,20 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.soundmeter.permission.PermissionScreen
 import com.example.soundmeter.ui.theme.SoundMeterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SoundMeterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                PermissionScreen()
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
             }
         }
     }
