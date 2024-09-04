6b66c1e399407c242ff0eb7614a12e3e8e12d363
yoonhee~ <flvps123@gmail.com>
2024-09-04 18:15:49 +0900

    [Feat] 구간별 그래프 라인 색 적용

--- app/src/main/java/com/example/soundmeter/screen/GraghUi.kt
+++ app/src/main/java/com/example/soundmeter/screen/GraghUi.kt
@@ -52,48 +52,7 @@ fun GraphUi() {
     var value by remember { mutableStateOf(0.0) }
     val modelProducer = remember { ChartEntryModelProducer() }
     val datasetForModel = remember { mutableStateListOf<FloatEntry>() }
-    val datasetLIneSpec by remember(value) {
-
-        mutableStateOf(
-            listOf(
-                LineChart.LineSpec(
-                    lineColor = when (value.toInt()) {
-                        in 0..40 -> Color(0xFF06D001).toArgb()
-                        in 41..70 -> Color.Yellow.toArgb()
-                        in 71..90 -> Color(0xFFFC4100).toArgb()
-                        in 91..1000 -> Color(0xFFE4003A).toArgb()
-                        else -> Color.Green.toArgb()
-                    },
-                    lineBackgroundShader = DynamicShaders.fromBrush(
-                        brush = Brush.verticalGradient(
-                            colors = when (value.toInt()) {
-                                in 0..40 -> listOf(
-                                    Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                                    Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                                )
-                                in 41..70 -> listOf(
-                                    Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                                    Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                                )
-                                in 71..90 -> listOf(
-                                    Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                                    Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                                )
-                                in 91..1000 -> listOf(
-                                    Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                                    Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                                )
-                                else -> listOf(
-                                    Color.Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-                                    Color.Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-                                )
-                            }
-                        )
-                    )
-                )
-            )
-        )
-    }
+    var datasetLIneSpec by remember{ mutableStateOf(emptyList<LineChart.LineSpec>()) }
 
     val scrollState = rememberChartScrollState()
 
@@ -123,51 +82,45 @@ fun GraphUi() {
             }
         }
     }
-//    LaunchedEffect(value) {
-//        datasetLIneSpec.clear()
-//        datasetLIneSpec.add(
-//            LineChart.LineSpec(
-//                lineColor = when (value.toInt()) {
-//                    in 0..40 -> Color(0xFF06D001).toArgb()
-//                    in 41..70 -> Color.Yellow.toArgb()
-//                    in 71..90 -> Color(0xFFFC4100).toArgb()
-//                    in 91..1000 -> Color(0xFFE4003A).toArgb()
-//                    else -> Color.Green.toArgb()
-//                },
-//                lineBackgroundShader = DynamicShaders.fromBrush(
-//                    brush = Brush.verticalGradient(
-//                        colors = when (value.toInt()) {
-//                            in 0..40 -> listOf(
-//                                Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-//                                Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-//                            )
-//
-//                            in 41..70 -> listOf(
-//                                Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-//                                Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-//                            )
-//
-//                            in 71..90 -> listOf(
-//                                Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-//                                Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-//                            )
-//
-//                            in 91..1000 -> listOf(
-//                                Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-//                                Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-//                            )
-//
-//                            else -> listOf(
-//                                Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
-//                                Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
-//                            )
-//                        }
-//                    )
-//                )
-//            )
-//        )
-//    }
-
+    LaunchedEffect(value) {
+        datasetLIneSpec = listOf(
+            LineChart.LineSpec(
+                lineColor = when (value.toInt()) {
+                    in 0..40 -> Color(0xFF06D001).toArgb()
+                    in 41..70 -> Color.Yellow.toArgb()
+                    in 71..90 -> Color(0xFFFC4100).toArgb()
+                    in 91..1000 -> Color(0xFFE4003A).toArgb()
+                    else -> Color.Green.toArgb()
+                },
+                lineBackgroundShader = DynamicShaders.fromBrush(
+                    brush = Brush.verticalGradient(
+                        colors = when (value.toInt()) {
+                            in 0..40 -> listOf(
+                                Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                                Color(0xFF06D001).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                            )
+                            in 41..70 -> listOf(
+                                Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                                Color.Yellow.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                            )
+                            in 71..90 -> listOf(
+                                Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                                Color(0xFFFC4100).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                            )
+                            in 91..1000 -> listOf(
+                                Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                                Color(0xFFE4003A).copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                            )
+                            else -> listOf(
+                                Color.Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_START),
+                                Color.Green.copy(alpha = com.patrykandpatrick.vico.core.DefaultAlpha.LINE_BACKGROUND_SHADER_END)
+                            )
+                        }
+                    )
+                )
+            )
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxWidth()
