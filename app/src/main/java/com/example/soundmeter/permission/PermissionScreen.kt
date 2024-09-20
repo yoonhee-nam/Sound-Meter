f12ce2c20f825d2067439c5a8bf4b701ac333d4f
yoonhee <flvps123@gmail.com>
2024-09-20 14:38:15 +0900

    [UI] 데시벨받아 미터기 반영

--- app/src/main/java/com/example/soundmeter/permission/PermissionScreen.kt
+++ app/src/main/java/com/example/soundmeter/permission/PermissionScreen.kt
@@ -8,6 +8,8 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -20,11 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.soundmeter.screen.GraphUi
+import com.example.soundmeter.screen.Meter
 import com.example.soundmeter.screen.PulsatingCircles
 
 @Composable
@@ -54,14 +58,23 @@ fun PermissionScreen() {
         }
     }
 
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
         if (hasAudioRecordPermission) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Meter()
+                GraphUi()
+            }
             //UI변경하기
-            GraphUi()
-            PulsatingCircles()
+
+//            PulsatingCircles()
+
 
         } else {
             Text(
